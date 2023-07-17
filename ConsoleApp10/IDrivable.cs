f3d4386c89a2a77bf28a5ce245c598af75b164de
leoca <leoca@DESKTOP-NVEVL9F>
2023-07-17 15:07:20 +0000

    Add project files.

---
+++ ConsoleApp10/IDrivable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    interface IDrivable
+    {
+        int Wheels { get; set; }
+        double Speed { get; set; }
+
+        void Move();
+        void Stop();
+    }
+}
---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp10
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Vehicle buick = new Vehicle("Buick",
+                4,160);
+            if (buick is IDrivable ) 
+            {
+                buick.Move();
+                buick.Stop();
+            } else
+            {
+                Console.WriteLine("The {0} can't be driven",
+                    buick.Brand);
+            }
+        }
+    }
+}
